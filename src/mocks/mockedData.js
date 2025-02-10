08c10414b69385a8068257d23ec8634d66172dc4
Paul Lockyer <plockyer@googlemail.com>
2025-02-10 09:48:57 +0000

    added mock meteorlogical data

---
+++ src/mocks/mockedData.js
@@ -0,0 +1,58 @@
+const chartingData = [
+    { name: "Monday", activeDevices: 5},
+    { name: "Tuesday", activeDevices: 8 },
+    { name: "Wednesday", activeDevices: 14},
+    { name: "Thursday", activeDevices: 6},
+    { name: "Friday", activeDevices: 3},
+    { name: "Saturday", activeDevices: 20},
+    { name: "Sunday", activeDevices: 18},
+];
+
+export const ticketData = [
+    { number: 100900, customer: "Billy Nottings", opened: "Dec 15th, 2024", status: "Overdue"},
+    { number: 100901, customer: "Sara Hortington", opened: "Jan 1st, 2025", status: "Open"},
+    { number: 100902, customer: "Milly Hilgerton", opened: "Jan 15th, 2025", status: "Open"},
+    { number: 100903, customer: "Tom Downs", opened: "Jab 15th, 2025", status: "On Hold"},
+    { number: 100904, customer: "Darren Mistings", opened: "Jan 25th, 2025", status: "On Hold"},
+    { number: 100905, customer: "Penolope Varnish", opened: "Jan 30th, 2025", status: "Open"},
+    { number: 100906, customer: "Tammy Godship", opened: "Feb 1st, 2025", status: "On Hold"},
+    { number: 100907, customer: "Fiona Sanderson", opened: "Nov 1st, 2024", status: "Over Due"},
+]
+
+export const meteorlogicalData = {
+    time: "2025-02-25T12:00",
+    temp: 5.6,
+    humidity: 76,
+    precipitation: 0.00,
+    rain: 0.00,
+    windSpeed: 10.4, 
+    windDir: 350,
+    historicalData: [
+        { time: "2025-01-25T00:00", temp: 5.9, humidity: 86, precipitation: 0.00, rain: 0.00, windSpeed: 18.9, windDir: 295},
+        { time: "2025-01-25T01:00", temp: 5.1, humidity: 86, precipitation: 0.00, rain: 0.00, windSpeed: 14.2, windDir: 297},
+        { time: "2025-01-25T02:00", temp: 4.1, humidity: 87, precipitation: 0.00, rain: 0.00, windSpeed: 16.4, windDir: 301},
+        { time: "2025-01-25T03:00", temp: 3.8, humidity: 83, precipitation: 0.00, rain: 0.00, windSpeed: 15.0, windDir: 300},
+        { time: "2025-01-25T04:00", temp: 3.5, humidity: 83, precipitation: 0.00, rain: 0.00, windSpeed: 13.9, windDir: 300},
+        { time: "2025-01-25T05:00", temp: 2.9, humidity: 84, precipitation: 0.00, rain: 0.00, windSpeed: 13.8, windDir: 303},
+        { time: "2025-01-25T06:00", temp: 2.6, humidity: 82, precipitation: 0.00, rain: 0.00, windSpeed: 12.5, windDir: 307},
+        { time: "2025-01-25T07:00", temp: 2.1, humidity: 82, precipitation: 0.00, rain: 0.00, windSpeed: 9.6, windDir: 311},
+        { time: "2025-01-25T08:00", temp: 1.7, humidity: 82, precipitation: 0.00, rain: 0.00, windSpeed: 8.7, windDir: 307},
+        { time: "2025-01-25T09:00", temp: 2.2, humidity: 81, precipitation: 0.00, rain: 0.00, windSpeed: 6.4, windDir: 320},
+        { time: "2025-01-25T10:00", temp: 3.9, humidity: 78, precipitation: 0.00, rain: 0.00, windSpeed: 2.1, windDir: 38},
+        { time: "2025-01-25T11:00", temp: 5.3, humidity: 79, precipitation: 0.00, rain: 0.00, windSpeed: 7.5, windDir: 134},
+        { time: "2025-01-25T12:00", temp: 6.3, humidity: 72, precipitation: 0.00, rain: 0.00, windSpeed: 7.6, windDir: 149},
+        { time: "2025-01-25T13:00", temp: 7.1, humidity: 63, precipitation: 0.00, rain: 0.00, windSpeed: 11.1, windDir: 223},
+        { time: "2025-01-25T14:00", temp: 7.2, humidity: 64, precipitation: 0.00, rain: 0.00, windSpeed: 11.2, windDir: 238},
+        { time: "2025-01-25T15:00", temp: 6.9, humidity: 70, precipitation: 0.00, rain: 0.00, windSpeed: 10.2, windDir: 236},
+        { time: "2025-01-25T16:00", temp: 6.4, humidity: 75, precipitation: 0.00, rain: 0.00, windSpeed: 6.8, windDir: 207},
+        { time: "2025-01-25T17:00", temp: 4.9, humidity: 82, precipitation: 0.00, rain: 0.00, windSpeed: 7.2, windDir: 193},
+        { time: "2025-01-25T18:00", temp: 4.8, humidity: 82, precipitation: 0.00, rain: 0.00, windSpeed: 10.4, windDir: 181},
+        { time: "2025-01-25T19:00", temp: 4.5, humidity: 84, precipitation: 0.00, rain: 0.00, windSpeed: 12.3, windDir: 192},
+        { time: "2025-01-25T20:00", temp: 4.4, humidity: 82, precipitation: 0.00, rain: 0.00, windSpeed: 13.5, windDir: 184},
+        { time: "2025-01-25T21:00", temp: 4.4, humidity: 81, precipitation: 0.00, rain: 0.00, windSpeed: 13.8, windDir: 187},
+        { time: "2025-01-25T22:00", temp: 4.9, humidity: 74, precipitation: 0.00, rain: 0.00, windSpeed: 12.7, windDir: 195},
+        { time: "2025-01-25T23:00", temp: 4.4, humidity: 76, precipitation: 0.00, rain: 0.00, windSpeed: 13.9, windDir: 178},
+    ]
+};
+
+export default chartingData;
