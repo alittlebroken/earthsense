7cd320ef8497f7ea91338f0c1bd0c0b3180e826c
Paul Lockyer <plockyer@googlemail.com>
2025-02-12 16:59:40 +0000

    updated to use card components

--- src/features/DashboardMain/DashboardMain.jsx
+++ src/features/DashboardMain/DashboardMain.jsx
@@ -3,6 +3,7 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,Responsiv
 import { meteorlogicalData} from '../../mocks/mockedData';
 
 import './DashboardMain.css';
+import { Card, CardBody, CardHeader, CardTitle } from '../../components/Card/Card';
 
 const DashboardMain = ({children}) => {
     return (
@@ -10,48 +11,78 @@ const DashboardMain = ({children}) => {
             
             <section className="grid grid-cols-4 gap-1">
                 
-                <div className="rounded bg-colour-white pad-1 flex flex-col flex-justify-between">
-                    <span className="text-700">Temperature</span>
-                    <span className="text-lg text-700">{meteorlogicalData.temp}°C</span>
-                </div>
-                <div className="rounded bg-colour-primary text-white pad-1 flex flex-col flex-justify-between">
-                    <span className="text-700">Humidity</span>
-                    <span className="text-lg text-700">{meteorlogicalData.humidity}%</span>
-                </div>
-                <div className="rounded bg-colour-secondary text-white pad-1 flex flex-col flex-justify-between">
-                    <span className="text-700">Wind Speed</span>
-                    <span className="text-lg text-700">{meteorlogicalData.windSpeed} km/h</span>
-                </div>
-                <div className="rounded bg-colour-tertiary pad-1 flex flex-col flex-justify-between">
-                    <span className="text-700">Wind Dir</span>
-                    <span className="text-lg text-700">{meteorlogicalData.windDir}</span>
-                </div>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Temperature</CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                        <span className="text-lg text-700">{meteorlogicalData.temp}°C</span>
+                    </CardBody>
+                </Card>
+
+                <Card primary>
+                    <CardHeader>
+                        <CardTitle>Humidity</CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                        <span className="text-lg text-700">{meteorlogicalData.humidity}%</span>
+                    </CardBody>
+                </Card>
+
+                <Card secondary>
+                    <CardHeader>
+                        <CardTitle>Wind Speed</CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                        <span className="text-lg text-700">{meteorlogicalData.windSpeed} km/h</span>
+                    </CardBody>
+                </Card>
+
+                <Card tertiary>
+                    <CardHeader>
+                        <CardTitle>Wind Dir</CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                        <span className="text-lg text-700">{meteorlogicalData.windDir}</span>
+                    </CardBody>
+                </Card>
+
             </section>
 
             <section className="grid grid-cols-4 gap-1 ">
 
-                <div className="grid-span-2 rounded bg-colour-white pad-1 grid gap-1">
-                    <span className="text-700">Temperature & Humidity - Last 24 Hours</span>
-                    <ResponsiveContainer width="90%" height="90%">
-                        <LineChart
-                            width={500}
-                            height={300}
-                            data={meteorlogicalData.historicalData}
-                        >
-                         <CartesianGrid strokeDasharray="3 3" />   
-                         <XAxis dataKey="time" />
-                         <YAxis yAxisId="left" />
-                         <YAxis yAxisId="right" orientation="right" />
-                         <Tooltip />
-                         <Legend />
-                         <Line yAxisId="left" type="monotone" dataKey="temp" stroke="#75485E" />
-                         <Line yAxisId="right" type="monotone" dataKey="humidity" stroke="#95BF8F" />
-                        </LineChart>
-                    </ResponsiveContainer>
-                </div>
+                <Card classes="grid-span-2">
+                   
+                    <CardHeader>
+                        <CardTitle>Temperature & Humidity - Last 24 Hours</CardTitle>
+                    </CardHeader>
 
-                <div className="grid-span-2 rounded bg-colour-white pad-1 grid gap-1">
-                    <span className="text-700">Wind Speed - Last 24 Hours</span>
+                        <ResponsiveContainer width="90%" height="90%">
+                            <LineChart
+                                width={500}
+                                height={300}
+                                data={meteorlogicalData.historicalData}
+                            >
+                            <CartesianGrid strokeDasharray="3 3" />   
+                            <XAxis dataKey="time" />
+                            <YAxis yAxisId="left" />
+                            <YAxis yAxisId="right" orientation="right" />
+                            <Tooltip />
+                            <Legend />
+                            <Line yAxisId="left" type="monotone" dataKey="temp" stroke="#75485E" />
+                            <Line yAxisId="right" type="monotone" dataKey="humidity" stroke="#95BF8F" />
+                            </LineChart>
+                        </ResponsiveContainer>
+                    
+                </Card>
+
+                <Card classes="grid-span-2 grid gap-1 pad-1">
+                   
+                    <CardHeader>
+                        <CardTitle>Wind Speed - Last 24 Hours</CardTitle>
+                    </CardHeader>
+
+                    <span className="text-700"></span>
                     <ResponsiveContainer width="90%" height="90%">
                         <LineChart
                             width={500}
@@ -67,42 +98,51 @@ const DashboardMain = ({children}) => {
                          <Line type="monotone" dataKey="windGusts" stroke="#FF1B1C" />
                         </LineChart>
                     </ResponsiveContainer>
-                </div>
-
+                    
+                </Card>
 
             </section>
 
             <section className="grid grid-cols-4 gap-1">
 
-                <div className="grid-span-4 rounded bg-colour-white pad-1 flex flex-col gap-1">
-                    <span className="text-700">Air Quality - Last 24 Hours</span>
-                    <div className="dataTableContainer">
-                        <table className="dataTable">
-                            <thead>
-                            <tr>
-                                <th>Time</th>
-                                <th>Particulates PM<sub>10</sub></th>
-                                <th>Carbon Monoxide CO</th>
-                                <th>Carbon Dioxide CO2</th>
-                                <th>Nitrogen Dioxide NO2</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                                {meteorlogicalData.airQualityData.map((air) => {
-                                    return (
-                                        <tr>
-                                            <td>{air.time}</td>
-                                            <td>{air.particulates}</td>
-                                            <td>{air.co}</td>
-                                            <td>{air.co2}</td>
-                                            <td>{air.no2}</td>
-                                        </tr>
-                                    )
-                                })}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                <Card classes="grid-span-4 gap-1">
+
+                    <CardHeader>
+                        <CardTitle>Air Quality - Last 24 Hours</CardTitle>
+                    </CardHeader>
+
+                    <CardBody>
+
+                        <div className="dataTableContainer">
+                            <table className="dataTable">
+                                <thead>
+                                <tr>
+                                    <th>Time</th>
+                                    <th>Particulates PM<sub>10</sub></th>
+                                    <th>Carbon Monoxide CO</th>
+                                    <th>Carbon Dioxide CO2</th>
+                                    <th>Nitrogen Dioxide NO2</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    {meteorlogicalData.airQualityData.map((air) => {
+                                        return (
+                                            <tr>
+                                                <td>{air.time}</td>
+                                                <td>{air.particulates}</td>
+                                                <td>{air.co}</td>
+                                                <td>{air.co2}</td>
+                                                <td>{air.no2}</td>
+                                            </tr>
+                                        )
+                                    })}
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </CardBody>
+
+                </Card>
 
             </section>
 
