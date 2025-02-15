3718b114a10e1fbdcc09a2efa0a3495d4b36ba56
Paul Lockyer <plockyer@googlemail.com>
2025-02-15 08:47:06 +0000

    set to use all available container space

--- src/features/DashChart/DashChart.jsx
+++ src/features/DashChart/DashChart.jsx
@@ -28,9 +28,14 @@ const DashChart = (props) => {
                        <CardTitle>{title}</CardTitle>
                    </CardHeader>
 
-                       <ResponsiveContainer width="90%" height="90%">
+                       <ResponsiveContainer width="100%" height="100%">
                            <LineChart
-                               
+                               margin={{
+                                top: 5,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                              }}
                                data={data[dataId]}
                            >
                            <CartesianGrid strokeDasharray="3 3" />   
