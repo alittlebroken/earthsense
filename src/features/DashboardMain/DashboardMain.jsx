6cc9207249c563f562652a8e80d15830d9d70e6c
Paul Lockyer <plockyer@googlemail.com>
2025-02-15 06:07:37 +0000

    replaced charts with new charting components

--- src/features/DashboardMain/DashboardMain.jsx
+++ src/features/DashboardMain/DashboardMain.jsx
@@ -5,6 +5,7 @@ import { meteorlogicalData} from '../../mocks/mockedData';
 import { Card, CardBody, CardHeader, CardTitle } from '../../components/Card/Card';
 
 import LatestReadings from '../LatestReadings/LatestReadings';
+import DashCharts from '../DashCharts/DashCharts';
 import AirQuality from '../AirQuality/AirQuality';
 
 import './DashboardMain.css';
@@ -15,59 +16,7 @@ const DashboardMain = ({children}) => {
             
             <LatestReadings data={meteorlogicalData} />
 
-            <section className="grid grid-cols-4 gap-1">
-
-                <Card classes="grid-span-2 pad-1">
-                   
-                    <CardHeader>
-                        <CardTitle>Temperature & Humidity - Last 24 Hours</CardTitle>
-                    </CardHeader>
-
-                        <ResponsiveContainer width="90%" height="90%">
-                            <LineChart
-                                width={500}
-                                height={300}
-                                data={meteorlogicalData.historicalData}
-                            >
-                            <CartesianGrid strokeDasharray="3 3" />   
-                            <XAxis dataKey="time" />
-                            <YAxis yAxisId="left" />
-                            <YAxis yAxisId="right" orientation="right" />
-                            <Tooltip />
-                            <Legend />
-                            <Line yAxisId="left" type="monotone" dataKey="temp" stroke="#75485E" />
-                            <Line yAxisId="right" type="monotone" dataKey="humidity" stroke="#95BF8F" />
-                            </LineChart>
-                        </ResponsiveContainer>
-                    
-                </Card>
-
-                <Card classes="grid-span-2 pad-1">
-                   
-                    <CardHeader>
-                        <CardTitle>Wind Speed - Last 24 Hours</CardTitle>
-                    </CardHeader>
-
-                    <span className="text-700"></span>
-                    <ResponsiveContainer width="90%" height="90%">
-                        <LineChart
-                            width={500}
-                            height={300}
-                            data={meteorlogicalData.historicalData}
-                        >
-                         <CartesianGrid strokeDasharray="3 3" />   
-                         <XAxis dataKey="time" />
-                         <YAxis />
-                         <Tooltip />
-                         <Legend />
-                         <Line type="monotone" dataKey="windSpeed" stroke="#FF7F11" />
-                         <Line type="monotone" dataKey="windGusts" stroke="#FF1B1C" />
-                        </LineChart>
-                    </ResponsiveContainer>
-                    
-                </Card>
-
-            </section>
+            <DashCharts />
 
             <AirQuality data={meteorlogicalData.airQualityData} />
 
