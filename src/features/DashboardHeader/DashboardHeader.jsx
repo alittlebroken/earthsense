a1d46dd10b11957937ecbadf4f2372b12743dec5
Paul Lockyer <plockyer@googlemail.com>
2025-02-28 14:53:51 +0000

    added menu burger icon for mobile view to show sidebar

--- src/features/DashboardHeader/DashboardHeader.jsx
+++ src/features/DashboardHeader/DashboardHeader.jsx
@@ -1,12 +1,28 @@
+import { SideMenuContext } from '../../contexts/SideMenuContext';
+import { useContext } from 'react';
+
+import { Icon } from '../../components/Icon/Icon';
+
 import './DashboardHeader.css';
 
 const DashboardHeader = () => {
+
+    const { menuOpen, setMenuOpen } = useContext(SideMenuContext);
+
+    const handleOnClick = (e) => {
+        e.preventDefault();
+        setMenuOpen(!menuOpen);
+    }
+
     return (
         <header className="dashboardHeaderContainer">
             <h2 className="roboto-heading h2">Dashboard</h2>
             <nav>
-
+                
             </nav>
+            <button onClick={handleOnClick}>
+                <Icon name="menu" classes="display-md-none display-lg-none" />
+            </button>
         </header>
     )
 };
